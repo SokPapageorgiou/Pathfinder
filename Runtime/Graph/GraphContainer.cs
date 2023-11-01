99f2607fe3a68835c8edfb36ca00b43773ff0a70
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2023-11-01 14:46:36 +0100

    Add GraphContainer

---
+++ Runtime/Graph/GraphContainer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Nodes;
+using UnityEngine;
+
+namespace Graph
+{
+    public class GraphContainer : MonoBehaviour
+    {
+        public List<Node<Vector3>> Nodes { get; private set; }
+
+        public void Initialize(List<Node<Vector3>> nodes)
+        {
+            Nodes = nodes;
+        }
+    }
+}
