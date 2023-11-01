70fccf5597b058314851ea01bf598e5394d5d9f3
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2023-11-01 14:47:45 +0100

    Add GraphInstantiator

---
+++ Editor/Graph/GraphInstantiator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Graph;
+using Nodes;
+using UnityEngine;
+
+namespace Editor.Graph
+{
+    public class GraphInstantiator
+    {
+        public void Instantiate(string name, List<Node<Vector3>> nodes)
+        {
+            var parent = new GameObject(name);
+            parent.AddComponent<GraphContainer>();
+        }
+    }
+}
