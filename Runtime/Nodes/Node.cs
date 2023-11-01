187394ca04f7bd37fd561c4a49a10c9395f2d068
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2023-11-01 14:47:29 +0100

    Refactor to make Set public

--- Runtime/Nodes/Node.cs
+++ Runtime/Nodes/Node.cs
@@ -4,7 +4,7 @@ namespace Nodes
 {
     public class Node<T>
     {
-        public T Value { get; private set; }
+        public T Value { get; set; }
         public List<Node<T>> Connections { get; private set; }
 
         public Node(T value, List<Node<T>> connections)
