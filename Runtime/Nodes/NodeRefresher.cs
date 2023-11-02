c06cd4f32286022a9d57c1cf6c38192b0bb1cefa
Sokratis Papageorgiou <hello@sokpapageorgiou.com>
2023-11-02 12:11:46 +0100

    Add node refresher

---
+++ Runtime/Nodes/NodeRefresher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Graph;
+using UnityEngine;
+
+namespace Nodes
+{
+    public class NodeRefresher
+    {
+        private readonly GraphContainer _graphContainer;
+
+        public NodeRefresher(GraphContainer graphContainer)
+        {
+            _graphContainer = graphContainer;
+        }
+
+        public void RefreshPosition(Node<Vector3>node, NodeVisualization nodeVisualization) 
+            => node.Value = nodeVisualization.transform.position;
+        
+        public void RefreshConnectionsFromNode(Node<Vector3>node, NodeVisualization nodeVisualization) 
+            => nodeVisualization.AddConnections( node.Connections
+                .Select(nodeConnection => _graphContainer.Nodes[nodeConnection]).ToList());
+
+        public void RefreshConnectionsToNode(Node<Vector3> node, List<NodeVisualization> connections)
+        {
+            node.Connections.Clear();
+            
+            connections.ForEach(connection => node.Connections
+                .Add(_graphContainer.Nodes.First(pair => pair.Value == connection).Key));
+        }
+    }
+}
